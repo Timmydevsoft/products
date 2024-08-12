5e4a2f8c51ee9dbe992c0fd77c4f9a7b33a0dfd0
Timmydevsoft <johnaytech@gmail.com>
2024-08-12 13:32:21 +0100

    New component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { Provider } from "react-redux";
 import cartStore from "./store/store";
-// import Index from "./component/Index";
 import "./index.css";
-import ProductList from "./component/ProductList";
+import Index from "./component/Index"
 
 export default function App() {
   return (
     <>
       <Provider store={cartStore}>
-        <ProductList/>
-        {/* <Index /> */}
+        <Index />
       </Provider>
     </>
   );
