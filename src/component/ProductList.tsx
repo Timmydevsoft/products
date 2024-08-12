5279ee3626b35247a88f2d02aa97a1c3bbc800f3
Timmydevsoft <johnaytech@gmail.com>
2024-08-12 22:18:12 +0100

    testingtwo

--- src/component/ProductList.tsx
+++ src/component/ProductList.tsx
@@ -73,6 +73,8 @@ const ProductList: React.FC = () => {
     [dispatch]
   );
 
+  console.log(dataBase);
+
   // console.log("working");
 
   useEffect(() => {
@@ -117,92 +119,14 @@ const ProductList: React.FC = () => {
     <div className="w-[100%] lg:w-[70%] overflow-y-scroll scroll">
       <h1 className="text-black text-3xl font-bold">Desserts</h1>
       <div className="text-red-500 text-3xl">Rendered</div>
-      <div className="">
-        {
-          dataBase.map((item:Prop, index:number)=>{
-            return(
-              <div key={index} className="">
-                <p className="text-green-400 text-2xl">{item.name}</p>
-                <p className="text-green-400 text-2xl">{item.image.mobile}</p>
-                <p className="text-green-400 text-2xl">{item.price}</p>
-              </div>
-            )
-          })
-        }
-      </div>
+      <div className="text-red-500 text-3xl">Rendered</div>
+      <div className="text-red-500 text-3xl">Rendered</div>
+      <div className="text-red-500 text-3xl">Rendered</div>
+      <div className="text-red-500 text-3xl">Rendered</div>
+      <div className="text-red-500 text-3xl">Rendered</div>
+     
 
-      
-      {/* <div className="container w-full">
-        <div className="flex flex-col lg:flex-row items-center gap-4 flex-wrap w-[100%]">
-          {dataBase.map((item: Part, index: number) => {
-            return (
-              <div key={index} className="w-[100%] lg:w-[32%]">
-                <div className="rounded-md w-[100%] h-[100%] py-4">
-                  <img
-                    className={`rounded-md w-full ${
-                      item.status === true ? "active-product" : ""
-                    }`}
-                    src={
-                      screeen <= 500
-                        ? item.image.mobile
-                        : screeen <= 768
-                        ? item.image.tablet
-                        : item.image.desktop
-                    }
-                    alt={`${item.name} image`}
-                  />
-                </div>
-                <div className="w-full flex justify-center">
-                  <div className="relative w-full flex justify-center">
-                    {!item.status ? (
-                      <button
-                        className="absolute -top-10 flex items-center justify-center gap-2 bg-white py-2 px-6 rounded-[50px] border border-burnt-rose text-base text-burnt-rose font-bold w-auto"
-                        onClick={() => {
-                          handleCart(item.id);
-                        }}
-                      >
-                        {" "}
-                        <img src={cart} alt="cart icon" /> {""}Add to cart
-                      </button>
-                    ) : (
-                      <div className="absolute -top-10 flex bg-redrose items-center justify-between px-4 py-2 w-[150px] rounded-full">
-                        <button
-                          onClick={() => {
-                            handleDecrement(item.id);
-                          }}
-                          className="flex items-center justify-center  bg-redrose font-bold rounded-[50%] w-[20px] h-[20px] border border-rose-50"
-                        >
-                          <img src={decrease} alt="decrement icon" />
-                        </button>
-                        <span className="text-white">{item.quantity}</span>
-                        <button
-                          onClick={() => {
-                            handleIncrement(item.id);
-                          }}
-                          className="flex items-center justify-center  bg-redrose font-bold rounded-[50%] w-[20px] h-[20px] border border-rose-50"
-                        >
-                          <img src={increase} alt="increment icon" />
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                </div>
-                <div className="mt-2 lg:mt-[2.5rem]">
-                  <p className="text-base text-[#ad8985] font-medium">
-                    {item.category}
-                  </p>
-                  <p className="text-base text-burnt-rose font-bold">
-                    {item.name}
-                  </p>
-                  <p className="text-base text-redrose font-medium">
-                    ${item.price}.00
-                  </p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div> */}
+     
      
     </div>
   );
